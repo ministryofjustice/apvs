a2938524d3cb2b24be37098a83c7fc8c875b6ed6
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-22 14:24:28 +0100

    Added the service name to the config.
    Implemented a basic placeholder version of the landing page.
    
    Fix spelling error in README.
    
    Dev Pair: Paul Wright

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # apvs
 Assisted Prison Visits Scheme
 
-Prototype implementation of the Assisted Prison Visits Scheme alpha. A bare bones implementation of the system indeded to assist with technical spikes
+Prototype implementation of the Assisted Prison Visits Scheme alpha. A bare bones implementation of the system intended to assist with technical spikes.
--- alpha/external-web/app/config.js
+++ alpha/external-web/app/config.js
@@ -3,8 +3,9 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
+
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Assisted Prison Visit Service',
 
   // Default port that prototype runs on
   port: '3000',
@@ -18,4 +19,4 @@ module.exports = {
   // Cookie warning - update link to service's cookie page.
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
 
-}
+};
