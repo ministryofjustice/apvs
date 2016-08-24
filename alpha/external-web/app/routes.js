07228f27edae70112b59746bd8bfb047d0a15c84
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-24 13:23:14 +0100

    chore/cleanup - Cleaning up elements of the gov.uk prototype kit.
    Removed the docs from both the internal and external sites.
    Removed example routes from both applications.
    Removed the example directory from both applications.
    Minor improvements to commenting.
    Added TODO comments for future improvements.

--- alpha/external-web/app/routes.js
+++ alpha/external-web/app/routes.js
@@ -1,38 +1,15 @@
 var express = require('express')
 var router = express.Router()
 
+module.exports = router
+
+// Render the landing page.
 router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Example routes - feel free to delete these
-
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name': 'Foo' })
-})
-
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18
-
-  if (over18 === 'false') {
-    // redirect to the relevant page
-    res.redirect('/examples/under-18')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
-  }
-})
-
-// add your routes here
-
-module.exports = router
-
 // TODO: Split out the elements here such that only the route is defined in this file.
+// TODO: Extract the database connection into another file.
 
 // Connect to a local Mongo DB
 var MongoClient = require('mongodb').MongoClient
--- alpha/internal-web/app/routes.js
+++ alpha/internal-web/app/routes.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+module.exports = router
+
+// TODO: Extract the database connection into another file.
+
 // Connect to a local Mongo DB
 var MongoClient = require('mongodb').MongoClient
 var db
@@ -12,7 +16,7 @@ MongoClient.connect('mongodb://localhost:27017/apvs', function (err, database) {
   }
 })
 
-// Get the list of claimants from the database.
+// Render the landing page with the claimants currently stored in the database.
 router.get('/', function (req, res) {
 
   db.collection('claimants').find().toArray(function (err, results) {
@@ -22,30 +26,3 @@ router.get('/', function (req, res) {
   })
 
 })
-
-// Example routes - feel free to delete these
-
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name': 'Foo' })
-})
-
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18
-
-  if (over18 === 'false') {
-    // redirect to the relevant page
-    res.redirect('/examples/under-18')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
-  }
-})
-
-// add your routes here
-
-module.exports = router
