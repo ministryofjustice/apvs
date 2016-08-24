09d26534f05254312dd06f11e038cf51a3e49545
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-24 14:18:50 +0100

    Chore/cleanup (#3) - Removing gov.uk prototype examples
    * apvs-002 - Initial commit for adding Redis database.
    
    Added dependency on node_redis.
    Updated gitignore to exlude IntelliJ iml files.
    Added connection to Redis database and example save.
    Added missing semi colons and set tabs to 4 spaces.
    
    Dev Pair: Paul Wright
    
    * apvs-002 - Implemented form persistence.
    
    Completed the form for submitting a claimants details.
    Added a route to service an add user request.
    Added a success page for adding a new claimant to the system.
    
    Dev Pair: Paul Wright
    
    * apvs-002 - Updated gitignore files
    
    A gitignore file is now specified within the external-web module. Will do the same for internal-web once created.
    Updated the top level gitignore to exclude all .ide directories, all .iml files, and all .vscode directories.
    
    * apvs-049 - Initial commit for Internal web.
    
    Committing a copy of the gov.uk prototype kit for the internal website housed in a new directory.
    Provided a gitignore file for this new module.
    Set the dependencies in package.json to include redis.
    Created a placeholder index.html page to serve as the landing page for internal users.
    Altered the configuration such that the internal site launches on port 3001 (versus the default 3000 which is being used by the external site).
    
    * apvs-049 - Code style cleanup to match StandardJS.
    
    Cleaned up gitignore files.
    
    * apvs-049 - Improved gitignore
    
    * apvs-049 - Pulling data from redis
    
    Index page displays user object which is pulled from redis database
    
    * apvs-002 - Merged in latest changes from UX prototype.
    
    * Updated mongo example
    
    * tom-mongo-testing - Got Mongo working as the database.
    
    Altered comma syntax to annoymous function syntax.
    Now prints content of the database each time a user is saved.
    Updated the package.json files to include mongodb and remove redis.
    
    * apvs-049 - Got Mongo working as the database.
    
    The internal site should now be able to pull from the Mongo database.
    Cleaned up semicolons and tabs spacing.
    
    * apvs-049 - Contents of the database now displayed on the internal landing page.
    
    Cleaned up usage of semicolons and 4 character spacing.
    
    * chore/cleanup - Cleaning up elements of the gov.uk prototype kit.
    
    Removed the docs from both the internal and external sites.
    Removed example routes from both applications.
    Removed the example directory from both applications.
    Minor improvements to commenting.
    Added TODO comments for future improvements.
    
    * chore/cleanup - Re-removing unused gov.uk elements
    
    Cleaning up elements that were reintroduced with merge, namely docs and examples folders

--- alpha/external-web/app/routes.js
+++ alpha/external-web/app/routes.js
@@ -1,44 +1,21 @@
 var express = require('express')
 var router = express.Router()
 
+module.exports = router
+
+// Render the landing page.
 router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Example routes - feel free to delete these
-
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name': 'Foo' })
-})
-
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18
-
-  if (over18 === 'false') {
-    // redirect to the relevant page
-    res.redirect('/examples/under-18')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
-  }
-})
-
-// add your routes here
-
-module.exports = router
-
 // TODO: Split out the elements here such that only the route is defined in this file.
+// TODO: Extract the database connection into another file.
 
 // Connect to a local Mongo DB
 var MongoClient = require('mongodb').MongoClient
 var db
 
-MongoClient.connect('mongodb://localhost:27017/apvs', function(err, database) {
+MongoClient.connect('mongodb://localhost:27017/apvs', function (err, database) {
   if (!err) {
     db = database
     console.log('Connected to MongoDB')
@@ -47,7 +24,6 @@ MongoClient.connect('mongodb://localhost:27017/apvs', function(err, database) {
 
 // Route to save a claimant to the system.
 router.post('/application_form', function (req, res) {
-
   // Test save of a claimant.
   db.collection('claimants').save(req.body, function (err, result) {
     if (!err) {
@@ -57,8 +33,9 @@ router.post('/application_form', function (req, res) {
 
   // Print out the current stored claimants of the database.
   db.collection('claimants').find().toArray(function (err, results) {
-    console.log('Database contents:');
-    console.log(results);
-  });
-
+    if (!err) {
+      console.log('Database contents:')
+      console.log(results)
+    }
+  })
 })
