e1755bd0118e8307adc60ca6b5d5708cea546f74
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-25 14:43:46 +0100

    apvs-049 - Split the database connection out into it's own file and included in the routes file for external web.
    Improved documentation around existing functions.

--- alpha/external-web/app/routes.js
+++ alpha/external-web/app/routes.js
@@ -1,41 +1,23 @@
 var express = require('express')
+var mongo = require('./database');
 var router = express.Router()
 
 module.exports = router
 
-// Render the landing page.
+/**
+ * Render the landing page.
+ */
 router.get('/', function (req, res) {
   res.render('index')
 })
 
-// TODO: Split out the elements here such that only the route is defined in this file.
-// TODO: Extract the database connection into another file.
-
-// Connect to a local Mongo DB
-var MongoClient = require('mongodb').MongoClient
-var db
-
-MongoClient.connect('mongodb://localhost:27017/apvs', function (err, database) {
-  if (!err) {
-    db = database
-    console.log('Connected to MongoDB')
-  }
-})
-
-// Route to save a claimant to the system.
-router.post('/application_form', function (req, res) {
-  // Test save of a claimant.
-  db.collection('claimants').save(req.body, function (err, result) {
-    if (!err) {
-      res.render('add_user_success')
-    }
-  })
-
-  // Print out the current stored claimants of the database.
-  db.collection('claimants').find().toArray(function (err, results) {
-    if (!err) {
-      console.log('Database contents:')
-      console.log(results)
+/**
+ * Save a single claimant to the system.
+ */
+router.post('/application_form', function (request, response) {
+  mongo.db.collection('claimants').save(request.body, function (error, result) {
+    if (!error) {
+      response.render('add_user_success')
     }
   })
 })
