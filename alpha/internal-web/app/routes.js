21d8deb9826c3246471e89bf22bd53a0f2912dd7
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-25 14:07:09 +0100

    apvs-049 - Exposed an endpoint to retrieve a claimant by id.
    Added new retrieve by id method.
    Cleaned up and improved the comments throughout.

--- alpha/internal-web/app/routes.js
+++ alpha/internal-web/app/routes.js
@@ -6,29 +6,52 @@ module.exports = router
 // TODO: Extract the database connection into another file.
 
 // Connect to a local Mongo DB
-var MongoClient = require('mongodb').MongoClient
+var MONGO = require('mongodb')
+var MONGO_CLIENT = MONGO.MongoClient
 var db
 
-MongoClient.connect('mongodb://localhost:27017/apvs', function (err, database) {
-  if (!err) {
+MONGO_CLIENT.connect('mongodb://localhost:27017/apvs', function (error, database) {
+  if (!error) {
     db = database
     console.log('Connected to MongoDB')
   }
 })
 
-// Render the landing page with the claimants currently stored in the database.
-router.get('/', function (req, res) {
-  res.render('index')
+/**
+ * Render the landing page with the claimants currently stored in the database.
+ */
+router.get('/', function (request, response) {
+  response.render('index')
 })
 
-// Return json object containing array of claimants
-router.get('/claimants', function (req, res) {
-  db.collection('claimants').find().toArray(function (err, results) {
-    if (!err) {
+/**
+ * Retrieve all claimants in the system.
+ */
+router.get('/claimants', function (request, response) {
+  db.collection('claimants').find().toArray(function (error, results) {
+    if (!error) {
+      console.log('Returning all claimants:')
       console.log(results)
-      // 'data' property needed for DataTable used to display claimants
-      var response = { data: results }
-      res.json(response)
+
+      // Append the 'data' property required by the DataTable library.
+      var claimants = { data: results }
+      response.json(claimants)
+    }
+  })
+})
+
+/**
+ * Retrieve a single claimant by their claimant ID.
+ */
+router.get('/claimant/:claimant_id', function (request, response) {
+  var id = new MONGO.ObjectID(request.params.claimant_id);
+  console.log('Retrieving claimant with ID: ' + id);
+
+  db.collection('claimants').find({ _id: id }).toArray(function (error, results) {
+    if (!error) {
+      console.log('Returning claimant:')
+      console.log(results)
+      response.json(results)
     }
   })
 })
