420d578bec1188d8f4c9f9287d5726e40f5d1f15
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-25 16:48:50 +0100

    apvs-049 - Code and comments tidy up.

--- alpha/external-web/app/routes.js
+++ alpha/external-web/app/routes.js
@@ -15,6 +15,20 @@ router.get('/', function (req, res) {
 })
 
 /**
+ * Render the about you income page.
+ */
+router.get('/about-your-income', function (req, res) {
+  res.render('about-your-income')
+})
+
+/**
+ * Render the application submitted page. Displayed after a successful file upload.
+ */
+router.get('/application-submitted', function (req, res) {
+  res.render('application-submitted')
+})
+
+/**
  * Save a single claimant to the system.
  */
 router.post('/application_form', function (request, response) {
@@ -26,28 +40,21 @@ router.post('/application_form', function (request, response) {
 })
 
 /**
- * Render the about you income page.
- */
-router.get('/about-your-income', function (req, res) {
-  res.render('about-your-income')
-})
-
-/**
- * Upload a single file to the system.
+ * Upload a single file to the system and it's meta data to the database.
  */
 router.post('/about-your-income', upload.single('evidence'), function (req, res) {
   if (req.file) {
     var metadata = {
       eligibilityId: req.file.filename, // using filename guild for temp id
-      // TODO: This should tie to a real claimant id.
-      claimId: null,
+      claimantID: null, // TODO: This should tie to a real claimant id.
       originalFilename: req.file.originalname,
       path: req.file.path
     }
 
+    // Save the uploaded files meta data to the database.
     mongo.db.collection('supporting-document').insertOne(metadata, function (err, result) {
       if (!err) {
-        console.log('Successfully saved file.');
+        console.log('Successfully saved file meta data.');
       }
     })
   }
@@ -55,8 +62,4 @@ router.post('/about-your-income', upload.single('evidence'), function (req, res)
   // TODO: validation
 
   res.redirect('/application-submitted')
-})
-
-router.get('/application-submitted', function (req, res) {
-  res.render('application-submitted')
 })
