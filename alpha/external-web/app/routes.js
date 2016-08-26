092dd4c65449a06ebecc3621137e5c305ad0d329
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-26 15:17:49 +0100

    apvs-002 - Separated about-you routes into new file and linked back to existing routes file.
    Removed add-user-success.html now using application-submitted instead.
    When submitting the about-you form the user is now redirected to the relationship page rather than to a success screen.

--- alpha/external-web/app/routes.js
+++ alpha/external-web/app/routes.js
@@ -1,10 +1,10 @@
 var express = require('express')
-var mongo = require('./database')
 var router = express.Router()
 
 module.exports = router
 
 // Include custom route files.
+require('./about-you');
 require('./relationship');
 require('./escorts');
 require('./about-your-income');
@@ -15,15 +15,4 @@ require('./application-submitted');
  */
 router.get('/', function (request, response) {
   response.render('index')
-})
-
-/**
- * Save a single claimant to the system.
- */
-router.post('/about-you', function (request, response) {
-  mongo.db.collection('claimants').insertOne(request.body, function (error, result) {
-    if (!error) {
-      response.render('add-user-success')
-    }
-  })
 })
