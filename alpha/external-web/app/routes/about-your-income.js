3b77dda4aec12814696f90809198e27a7af4ab15
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-29 12:28:51 +0100

    apvs-002 - Implemented form functionality for the about-your-income page.
    Implemented get and post routes for the page.
    Implemented the required form elements and tied them back to the persisted document for when the page is loaded.
    Added ifs for each of the radio buttons to set them to the required value on page load.
    Made the formatting of the HTML consistent.

--- alpha/external-web/app/routes/about-your-income.js
+++ alpha/external-web/app/routes/about-your-income.js
@@ -12,26 +12,41 @@ var upload = multer({ dest: 'eligibility-uploads/' })
 var mongo = require('../database')
 
 /**
- * Render the about you income page.
+ * Render the about you about-your-income page and populate with the details of the claimant with the given ID.
+ *
+ * Example call: http://localhost:3000/about-your-income/57c3f1139e03be003bfac1aa
  */
-router.get('/about-your-income', function (request, response) {
-  response.render('about-your-income')
+router.get('/about-your-income/:claimant_id', function (request, response) {
+  var id = new mongo.client.ObjectID(request.params.claimant_id)
+  console.log('GET /about-your-income/' + id + ' called.')
+
+  mongo.db.collection('claimants').findOne({ _id: id }, function (error, claimant) {
+    if (!error) {
+      response.render('about-your-income', { 'claimant': claimant })
+    }
+  })
 })
 
 /**
  * Upload a single file to the system and it's meta data to the database.
+ *
+ * Example call: http://localhost:3000/about-your-income/57c3f1139e03be003bfac1aa
  */
-router.post('/about-your-income', upload.single('evidence'), function (request, response) {
+router.post('/about-your-income/:claimant_id', upload.single('evidence'), function (request, response) {
+  var id = new mongo.client.ObjectID(request.params.claimant_id)
+  console.log('POST /about-your-income/' + id + ' called.')
+
+  // Bundle the file meta data into an object.
   if (request.file) {
     var metadata = {
       eligibilityId: request.file.filename, // using filename guild for temp id
-      claimantID: null, // TODO: This should tie to a real claimant id.
       originalFilename: request.file.originalname,
       path: request.file.path
     }
 
-    // Save the uploaded files meta data to the database.
-    mongo.db.collection('supporting-document').insertOne(metadata, function (error, result) {
+    // Save the uploaded files meta data to the claimant.
+    mongo.db.collection('claimants').updateOne({ _id: id }, { $set: metadata }, function (error, result) {
+      console.log(result)
       if (!error) {
         console.log('Successfully saved file meta data.')
       }
