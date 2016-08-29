6a182231a41a266a74b8940f3b562df74851ed3f
Thomas Rowe <thomasrowe101@gmail.com>
2016-08-29 09:41:12 +0100

    apvs-002 - Documentation improvements
    Added example calls for the routes.
    Reorganised the code to list the gets, and then the posts.

--- alpha/external-web/app/routes/about-you.js
+++ alpha/external-web/app/routes/about-you.js
@@ -15,19 +15,9 @@ router.get('/about-you', function (request, response) {
 })
 
 /**
- * Save a single claimant to the system.
- */
-router.post('/about-you', function (request, response) {
-  console.log('POST /about-you called.')
-  mongo.db.collection('claimants').insertOne(request.body, function (error, result) {
-    if (!error) {
-      response.redirect('/relationship')
-    }
-  })
-})
-
-/**
  * Render the about you page with an existing claimant's details if they exist.
+ *
+ * Example call: http://localhost:3000/about-you/57c3f1139e03be003bfac1aa
  */
 router.get('/about-you/:claimant_id', function (request, response) {
   console.log('GET /about-you/' + request.params.claimant_id + ' called.')
@@ -41,7 +31,21 @@ router.get('/about-you/:claimant_id', function (request, response) {
 })
 
 /**
- * Update an existing claimant in the system.
+ * Save a single claimant to the system.
+ */
+router.post('/about-you', function (request, response) {
+  console.log('POST /about-you called.')
+  mongo.db.collection('claimants').insertOne(request.body, function (error, result) {
+    if (!error) {
+      response.redirect('/relationship')
+    }
+  })
+})
+
+/**
+ * Update a single existing claimant.
+ *
+ * Example call: http://localhost:3000/about-you/57c3f1139e03be003bfac1aa
  */
 router.post('/about-you/:claimant_id', function (request, response) {
   console.log('POST /about-you/' + request.params.claimant_id + ' called.')
