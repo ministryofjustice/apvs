25a6bf9c0fac4aac9b4c6ac619e4deca3a04cd51
Thomas Rowe <thomasrowe101@gmail.com>
2016-09-05 16:05:09 +0100

    Refactored eligibility-client
    - We now export each function individually rather than grouping them into an object.
    - The advantage to this approach is that the methods can now call each other whereas with the previous object approach they could not.

--- alpha/internal-web/app/eligibility-client.js
+++ alpha/internal-web/app/eligibility-client.js
@@ -1,82 +1,78 @@
 // Include in the database.
 var mongo = require('../app/database')
 
-module.exports = {
+/**
+ * Retrieve all claimants from the database.
+ * @param callback A callback defining what to do after a successful and failed get.
+ */
+exports.getAll = function (callback) {
+  mongo.db.collection('claimants').find().toArray(function (error, claimant) {
+    if (!error) {
+      callback(null, claimant)
+    } else {
+      callback(error, null)
+    }
+  })
+}
 
-  /**
-   * Retrieve all claimants from the database.
-   * @param callback A callback defining what to do after a successful and failed get.
-   */
-  getAll: function (callback) {
-    mongo.db.collection('claimants').find().toArray(function (error, claimant) {
-      if (!error) {
-        callback(null, claimant)
-      } else {
-        callback(error, null)
-      }
-    })
-  },
+/**
+ * Retrieve a single claimant from the database by claimant ID.
+ * @param id The id of the claimant to retrieve.
+ * @param callback A callback defining what to do after a successful and failed get.
+ */
+exports.get = function (id, callback) {
+  mongo.db.collection('claimants').findOne({ _id: exports.mongoId(id) }, function (error, claimant) {
+    if (!error) {
+      callback(null, claimant)
+    } else {
+      callback(error, null)
+    }
+  })
+}
 
-  /**
-   * Retrieve a single claimant from the database by claimant ID.
-   * @param id The id of the claimant to retrieve.
-   * @param callback A callback defining what to do after a successful and failed get.
-   */
-  get: function (id, callback) {
-    mongo.db.collection('claimants').findOne({ _id: mongoId(id) }, function (error, claimant) {
-      if (!error) {
-        callback(null, claimant)
-      } else {
-        callback(error, null)
-      }
-    })
-  },
+/**
+ * Save a new claimant to the database.
+ * @param claimant The claimant details to save.
+ * @param callback A callback defining what to do after a successful and failed save.
+ */
+exports.save = function (claimant, callback) {
+  mongo.db.collection('claimants').insertOne(claimant, function (error, savedClaimant) {
+    if (!error) {
+      callback(null, savedClaimant)
+    } else {
+      callback(error, null)
+    }
+  })
+}
 
-  /**
-   * Save a new claimant to the database.
-   * @param claimant The claimant details to save.
-   * @param callback A callback defining what to do after a successful and failed save.
-   */
-  save: function (claimant, callback) {
-    mongo.db.collection('claimants').insertOne(claimant, function (error, savedClaimant) {
-      if (!error) {
-        callback(null, savedClaimant)
-      } else {
-        callback(error, null)
-      }
-    })
-  },
+/**
+ * Update an existing claimant.
+ * @param id The id of the claimant to update.
+ * @param claimant The claimant details to use for the update.
+ * @param callback A callback defining what to do after a successful and failed update.
+ */
+exports.update = function (id, claimant, callback) {
+  mongo.db.collection('claimants').updateOne({ _id: exports.mongoId(id) }, { $set: claimant }, function (error, updatedClaimant) {
+    if (!error) {
+      callback(null, updatedClaimant)
+    } else {
+      callback(error, null)
+    }
+  })
+}
 
-  /**
-   * Update an existing claimant.
-   * @param id The id of the claimant to update.
-   * @param claimant The claimant details to use for the update.
-   * @param callback A callback defining what to do after a successful and failed update.
-   */
-  update: function (id, claimant, callback) {
-    mongo.db.collection('claimants').updateOne({ _id: mongoId(id) }, { $set: claimant }, function (error, updatedClaimant) {
-      if (!error) {
-        callback(null, updatedClaimant)
-      } else {
-        callback(error, null)
-      }
-    })
-  },
-
-  /**
-   * Cleans the persistence store.
-   * @param callback A callback defining what to do after a successful and failed update.
-   */
-  drop: function (callback) {
-    mongo.db.dropCollection('claimants', function (error, updatedClaimant) {
-      if (!error) {
-        callback(null)
-      } else {
-        callback(error)
-      }
-    })
-  }
-
+/**
+ * Cleans the persistence store.
+ * @param callback A callback defining what to do after a successful and failed update.
+ */
+exports.drop = function (callback) {
+  mongo.db.dropCollection('claimants', function (error, updatedClaimant) {
+    if (!error) {
+      callback(null)
+    } else {
+      callback(error)
+    }
+  })
 }
 
 /**
@@ -84,6 +80,6 @@ module.exports = {
  * @param id The string to wrap as a Mongo ID.
  * @returns {ObjectID} A Mongo ObjectID suitable for making queries into a Mongo database.
  */
-function mongoId (id) {
+exports.mongoId = function (id) {
   return new mongo.client.ObjectID(id)
 }
