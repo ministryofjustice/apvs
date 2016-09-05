9cf1c70b86d0c14af7fb5dbd76002edbe810e19d
Thomas Rowe <thomasrowe101@gmail.com>
2016-09-05 13:27:10 +0100

    apvs-118 - Factored out the functions related to the isEligibilityModified Flag into a new file.
    - Created the eligibility-flag.js file to house the functions related to the isEligibilityModified flag.
    - Required the new file into claim.js.

--- alpha/external-web/app/routes/claim.js
+++ alpha/external-web/app/routes/claim.js
@@ -2,7 +2,7 @@
  * This file defines all routes for the claim page.
  */
 var router = require('../routes')
-var client = require('../eligibility-client')
+var eligibilityFlag = require('../services/eligibility-flag')
 
 /**
  * Renders the claim page.
@@ -34,51 +34,13 @@ router.post('/claim', function (request, response) {
     console.log('Request Failed! Please fully complete the form.')
     response.status(500).render('error', { error: 'Request Failed! Please fully complete the form.' })
   } else {
-    updateEligibilityFlag(id, eligibility)
+    eligibilityFlag.update(id, eligibility)
 
     // Redirect the user based on the response to the details changed question.
-    if (isEligibilityModified(eligibility)) {
+    if (eligibilityFlag.isModified(eligibility)) {
       response.redirect('/about-you/' + id)
     } else {
       response.redirect('/claim-details/' + id)
     }
   }
 })
-
-/**
- * Determine if the given eligibility value equates to true or false.
- * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
- * @returns {boolean} True if eligibility is equal to 'Yes', false otherwise.
- */
-function isEligibilityModified (eligibility) {
-  return eligibility === 'Yes'
-}
-
-/**
- * Builds the object to persist to the database that holds the flag indicating if the claimants eligibility is new or
- * old.
- * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
- * @returns {{isEligibilityModified: boolean}} An object who's value indicates if the claimants eligibility is new or
- *          old.
- */
-function buildEligibilityFlag (eligibility) {
-  return {
-    'isEligibilityModified': isEligibilityModified(eligibility)
-  }
-}
-
-/**
- * Adds or updates the isEligibilityModified flag of the claimant with the given id.
- * @param id The id of the claimant that this flag is associated with.
- * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
- */
-function updateEligibilityFlag (id, eligibility) {
-  client.update(id, buildEligibilityFlag(eligibility), function (error, claimant) {
-    if (!error) {
-      console.log('Successfully modified the isEligibilityModified flag for claimant with id: ' + id)
-    } else {
-      console.log('Failed to modify the isEligibilityModified flag for claimant with id: ' + id)
-      response.status(500).render('error', { message: error.message, error: error })
-    }
-  })
-}
---
+++ alpha/external-web/app/services/eligibility-flag.js
@@ -0,0 +1,47 @@
+/**
+ * Defines functions to support building, retrieving, and persisting the isEligibilityModified flag.
+ *
+ * This flag is intended to indicate whether an eligibility claim is brand new or a modification:
+ * - A new claim is one that is started via the 'Start Now' button on the index page with no ID given.
+ * - A modification occurs when a claimant indicates their existing eligibility details have changed when submitting a
+ *   claim via the 'Claim Now button on the index page'.
+ */
+var client = require('../eligibility-client')
+
+/**
+ * Determine if the given eligibility value equates to true or false.
+ * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
+ * @returns {boolean} True if eligibility is equal to 'Yes', false otherwise.
+ */
+exports.isModified = function (eligibility) {
+  return eligibility === 'Yes'
+}
+
+/**
+ * Builds the object to persist to the database that holds the flag indicating if the claimants eligibility is new or
+ * old.
+ * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
+ * @returns {{isEligibilityModified: boolean}} An object who's value indicates if the claimants eligibility is new or
+ *          old.
+ */
+exports.build = function (eligibility) {
+  return {
+    'isEligibilityModified': exports.isModified(eligibility)
+  }
+}
+
+/**
+ * Adds or updates the isEligibilityModified flag of the claimant with the given id.
+ * @param id The id of the claimant that this flag is associated with.
+ * @param eligibility {String} A String that is expected to be either 'Yes' or 'No'
+ */
+exports.update = function (id, eligibility) {
+  client.update(id, exports.build(eligibility), function (error, claimant) {
+    if (!error) {
+      console.log('Successfully modified the isEligibilityModified flag for claimant with id: ' + id)
+    } else {
+      console.log('Failed to modify the isEligibilityModified flag for claimant with id: ' + id)
+      response.status(500).render('error', { message: error.message, error: error })
+    }
+  })
+}
