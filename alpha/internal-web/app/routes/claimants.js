cb13a62c53a48cc389fe463a4647546239ebe02d
Thomas Rowe <thomasrowe101@gmail.com>
2016-09-07 11:39:27 +0100

    Replaced console.log lines across internal web.

--- alpha/internal-web/app/routes/claimants.js
+++ alpha/internal-web/app/routes/claimants.js
@@ -1,34 +1,33 @@
 var router = require('../routes')
 var client = require('../eligibility-client')
+var logger = require('../services/bunyan-logger').logger
 
 router.get('/claimants', function (request, response) {
-  console.log('GET /claimants/ called.')
+  logger.info({request: request})
 
   client.getAll(function (error, claimants) {
     if (!error) {
-      console.log('Returning all claimants.')
+      logger.info('Returning all claimants.')
 
       // Append 'data' to the JSON object. Required by the DataTable library.
       var jsonClaimants = { data: claimants }
       response.json(jsonClaimants)
     } else {
-      console.log('Failed to retrieve claimants.')
+      logger.error('Failed to retrieve claimants.')
       response.status(500).render('error', { message: error.message, error: error })
     }
   })
 })
 
 router.get('/claimants/:claimant_id', function (request, response) {
-  var id = request.params.claimant_id
-  console.log('GET /claimants/' + id + ' called.')
+  logger.info({request: request})
 
+  var id = request.params.claimant_id
   client.get(id, function (error, claimant) {
     if (!error) {
-      console.log('Returning claimant:')
-      console.log(claimant)
       response.json(claimant)
     } else {
-      console.log('Failed to retrieve claimants')
+      logger.error('Failed to retrieve claimants')
       response.status(500).render('error', { message: error.message, error: error })
     }
   })
--- alpha/internal-web/app/routes/index.js
+++ alpha/internal-web/app/routes/index.js
@@ -1,19 +1,21 @@
 var router = require('../routes')
 var client = require('../eligibility-client')
+var logger = require('../services/bunyan-logger').logger
 
 router.get('/', function (request, response) {
+  logger.info({request: request})
   response.render('index')
 })
 
 router.get('/clean', function (request, response) {
-  console.log('GET /clean called.')
+  logger.info({request: request})
 
   client.drop(function (error) {
     if (!error) {
-      console.log('Cleared all.')
+      logger.info('Cleared all.')
       response.redirect('/')
     } else {
-      console.log('Failed to clean.')
+      logger.warn('Failed to clean.')
       response.redirect('/')
     }
   })
