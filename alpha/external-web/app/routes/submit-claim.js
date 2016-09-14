034721ff8a534aed2103d4c97d74fd1fdcb8e5f5
Paul Wright <wrightpaul08@gmail.com>
2016-09-14 15:53:36 +0100

    Feature/apvs 010/declare visit and upload stamp (#60)
    * apvs-010 - Added declare-your-visit page
    
    * apvs-0126 - upload visit stamp and submit-claim.html
    
    - Call to claim check API moved to submit-claim.html
    - declare-your-visit sits in between claim-details and submit-claim
    
    * apvs-010 - adding separate folder for visit stamps
    
    Fixed logic in declare-your-visit POST route
    
    * apvs-010 - adding visit-stamp-uploads directory
    
    * Applying code review comments
    
    * fixed evidence download bug
    
    * Re-using reference checker to check id is valid

---
+++ alpha/external-web/app/routes/submit-claim.js
@@ -0,0 +1,43 @@
+var router = require('../routes')
+var externalApiRest = require('../services/external-api-rest')
+var reference = require('../services/reference-checker')
+
+const claimantsCollection = 'claimants'
+
+router.get('/submit-claim/:claimant_id', function (request, response, next) {
+  var id = request.params.claimant_id
+  reference.isValid(id, claimantsCollection)
+    .then(function (isValid) {
+      response.render('submit-claim', { 'id': id })
+    })
+    .catch(function (error) {
+      response.status(500).render('error', { message: error.message, error: error })
+    })
+  next()
+})
+
+router.post('/submit-claim/:claimant_id', function (request, response, next) {
+  var amount = request.query.amount ? parseInt(request.query.amount) : 99
+  externalApiRest.checkForAutomaticProcessing({ amount: amount })
+    .then(function (result) {
+      if (result['processing-type'] === 'automatic') {
+        response.redirect('/claim-automatic')
+      } else {
+        response.redirect('/claim-manual')
+      }
+    })
+    .catch(function (error) {
+      response.status(500).render('error', {message: error.message, error: error})
+    })
+  next()
+})
+
+router.get('/claim-automatic', function (request, response, next) {
+  response.render('claim-automatic')
+  next()
+})
+
+router.get('/claim-manual', function (request, response, next) {
+  response.render('claim-manual')
+  next()
+})
