8da7ab5c6079a3e2d7f9b5e7adebfa19998f4100
Thomas Swann <ts.swann@gmail.com>
2016-09-14 15:51:54 +0100

    Added dev mail server container and notification to worker process

--- alpha/notify-worker/app/process-notifications.js
+++ alpha/notify-worker/app/process-notifications.js
@@ -1,15 +1,33 @@
-
 var logger = require('./bunyan-logger')
 var client = require('./db-client')
-var _ = require('lodash')
+var mailer = require('./mailer')
+var Promise = require('bluebird')
+
+const EMAIL_CONTACT_METHOD = 'email'
 
 exports.run = function () {
   client.getPendingNotifications()
     .then(function (notifications) {
-      logger.info(notifications)
-      _(notifications).forEach(value => logger.info(value))
+      if (notifications) {
+        Promise.all(notifications.map(function (notification) {
+          processNotification(notification)
+        }))
+      }
     })
     .catch(function (error) {
       logger.error(error)
     })
+}
+
+var processNotification = function (notification) {
+  logger.info(notification)
+
+  var action = notification.action
+
+  if (action.method === EMAIL_CONTACT_METHOD) {
+    logger.info('Sending email to recipient: ' + action.recipient)
+    mailer.sendMail(action.reference, action.recipient)
+  } else {
+    logger.info('No action for contact preferene ' + action.method)
+  }
 }
