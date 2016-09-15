8cfb46928df6baa26dbc1fa85fcd9e79996be528
Thomas Swann <ts.swann@gmail.com>
2016-09-15 22:18:14 +0100

    Removed beforeEach

--- alpha/notify-worker/test/app/test-process-tasks.js
+++ alpha/notify-worker/test/app/test-process-tasks.js
@@ -1,4 +1,4 @@
-/* global describe it beforeEach */
+/* global describe it */
 var proxyquire = require('proxyquire')
 var sinon = require('sinon')
 var client = require('../../app/db-client')
@@ -7,37 +7,36 @@ var mailer = require('../../app/mailer')
 require('sinon-bluebird')
 
 describe('process-tasks', function () {
-  var processTasks, dummyPendingTask, processNotification
+  var processTasks, processNotification
 
-  beforeEach(function () {
-    dummyPendingTask = {
-      name: 'application-notification',
-      status: 'PENDING',
-      action: {
-        method: 'email',
-        email: 'test@test.com',
-        reference: 'APVS12345'
-      }
+  var dummyPendingTask = {
+    name: 'application-notification',
+    status: 'PENDING',
+    action: {
+      method: 'email',
+      email: 'test@test.com',
+      reference: 'APVS12345'
     }
-
-    processNotification = {
-      process: function (notification) {
-        console.log(notification)
-      }
-    }
-
-    processTasks = proxyquire('../../app/process-tasks', {
-      './db-client': client,
-      './tasks/notification': processNotification,
-      './bunyan-logger': logger
-    })
-  })
+  }
 
   describe('run', function () {
     it('should run notification task twice', function () {
+      processNotification = {
+        process: function (notification) {
+          console.log(notification)
+        }
+      }
+
+      processTasks = proxyquire('../../app/process-tasks', {
+        './db-client': client,
+        './tasks/notification': processNotification,
+        './bunyan-logger': logger
+      })
+
       sinon.stub(client, 'getPendingNotifications').resolves([dummyPendingTask, dummyPendingTask])
       sinon.stub(client, 'setTaskComplete')
       sinon.stub(mailer, 'sendMail')
+
       processTasks.run()
     })
   })
